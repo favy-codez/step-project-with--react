a725af82beb48f3d32f5754c354d92a7194e4560
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-07-01 08:22:24 +0100

    added close button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function prevButton(){
     if (step > 1) setStep(step - 1);
@@ -19,27 +20,36 @@ function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+    <div>
+      <button className='close' onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+
+      { isOpen && (
+        <div className="steps">
+        <div className="numbers">
+          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        </div>
+  
+        <p className='message'> Step {step}: {messages[step -1]}</p>
+  
+        <div className='buttons'>
+          <button style={{backgroundColor:"#7950f2", color:"#fff"}} 
+          onClick={prevButton}>
+            Previous</button>
+          <button style={{backgroundColor:"#7950f2", color:"#fff"}} 
+          // passing in the fxn value, calling it would require adding ()
+          onClick={nextButton}>
+            Next</button>
+        </div>
+  
       </div>
-
-      <p className='message'> Step {step}: {messages[step -1]}</p>
-
-      <div className='buttons'>
-        <button style={{backgroundColor:"#7950f2", color:"#fff"}} 
-        onClick={prevButton}>
-          Previous</button>
-        <button style={{backgroundColor:"#7950f2", color:"#fff"}} 
-        // passing in the fxn value, calling it would require adding ()
-        onClick={nextButton}>
-          Next</button>
-      </div>
-
+    )
+      }
     </div>
-  );
+    );
 }
 
 export default App;
